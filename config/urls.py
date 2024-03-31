3741c5352e01d7779055b22d291cbe9a8e5917de
Ivan Skliaruk <cunanori@gmail.com>
2024-03-31 22:04:01 +0200

    empty index.html added

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from catalog.views import default
 
 urlpatterns = [
+    path('', default, name='home'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
